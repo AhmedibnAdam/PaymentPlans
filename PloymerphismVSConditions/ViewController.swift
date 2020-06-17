74773e346d07e33d3c99c64f4ba1de673d3a7021
Ahmed_Adam <ibn.abuadam@gmail.com>
2020-06-17 23:55:31 +0200

    Initial Commit

---
+++ PloymerphismVSConditions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PloymerphismVSConditions
+//
+//  Created by Adam on 6/17/20.
+//  Copyright © 2020 TestApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
